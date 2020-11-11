18e73a5926bf2ead6d53ebfe1deebca4a3182abf
zhou xin <zhouxin312@qq.com>
2020-11-11 16:27:25 +0800

    temporary commit

---
+++ chapter-4/section-4.3/tree.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tree.h"
+struct TreeNode
+{
+	ElementType Element;
+	SearchTree Left;
+	SearchTree Right;
+};
+
+static void FatalError(char *S)
+{
+	fputs("S\n",stdout);
+	exit(EXIT_FAILURE);
+}
+static void Error(char *S)
+{
+	puts(S);
+}
+
+SearchTree MakeEmpty(SearchTree T)
+{
+	if(T!=NULL)
+	{
+		MakeEmpty(T->Left);
+		MakeEmpty(T->Right);
+		free(T);
+	}
+	return NULL;
+}
+
+Position Find(ElementType X,SearchTree T)
+{
+	Position P=T;
+	if(P!=NULL)
+	{
+		if(P->Element<X)
+			Find(X,P->Right);
+		else if(P->Element>X)
+			Find(X,P->Left);
+		else
+			return P;
+	}
+	return P;
+}
+
+Position FindMin(SearchTree T)
+{
+	Position P=T;
+	if(P==NULL)
+		return NULL;
+	if(P->Left!=NULL)
+		return FindMin(P->Left);
+	else
+		return P;
+}
+
+Position FindMax(SearchTree T)
+{
+	Position P=T;
+	if(P==NULL)
+		return NULL;
+	if(P->Right!=NULL)
+		return FindMax(P->Right);
+	else
+		return P;
+}
+
+SearchTree Insert(ElementType X,SearchTree T)
+{
+	if(T==NULL)
+	{
+		T=malloc(sizeof(struct TreeNode));
+		if(T==NULL)
+			FatalError("Out of space!");
+		else
+		{
+			T->Element=X;
+			T->Left=T->Right=NULL;
+		}
+	}
+	else
+	if(T->Element>X)
+		T->Left=Insert(X,T->Left);
+	else
+	if(T->Element<X)
+		T->Right=Insert(X,T->Right);
+/*	else
+
+if T->Element==X,there will do nothing
+*/
+	return T;
+}
+
+SearchTree Delete(ElementType X,SearchTree T)
+{
+	Position Tmpcell;
+	if(T==NULL)
+		Error("Element not found!");
+	else
+	if(X>T->Element)
+		T->Right=Delete(X,T->Right);
+	else
+	if(X<T->Element)
+		T->Left=Delete(X,T->Left);
+	else
+	{
+		if(T->Left && T->Right)
+		{
+			Tmpcell=FindMin(T->Right);
+			T->Element=Tmpcell->Element;
+			T->Right=Delete(T->Element,T->Right);
+		}
+		else
+		{
+			Tmpcell=T;
+			if(T->Left==NULL)
+				T=T->Right;
+			else
+			if(T->Right==NULL)
+				T=T->Left;
+			free(Tmpcell);
+		}
+	}
+	return T;
+}
+
+ElementType Retrieve(Position P)
+{
+	if(P==NULL)
+		Error("Erorr position!");
+	else
+		return P->Element;
+}
