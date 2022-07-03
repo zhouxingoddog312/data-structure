0f71cdafaf7d46473c5066b894e85e28a994313f
zhouxin <zhouxin312@qq.com>
2022-07-03 21:35:23 +0800

    reverse link complete

--- mooc-chenyue/program/4_fix.c
+++ mooc-chenyue/program/4_fix.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define MaxSize 100000
+#define MaxSize 100000//用来模拟0～99999的链表
 struct Node
 {
 	int Data;
@@ -7,19 +7,19 @@ struct Node
 };
 typedef struct Node node;
 typedef struct Node *ptn;
-node Array[MaxSize+1];
+node Array[MaxSize+1];//多一个元素，最后一个元素用来作为head
 void PrintLink(int Link);
-int RecieveLink(int *link_len,int *K);
-int Reverse(int head,int K,int *tmp_link);
+int RecieveLink(int *link_len,int *K);//这里的link_len代表的是链表的长度，因为有的数据不在链表内
+int Reverse(int head,int K,int *tmp_link);//这里tmp_link用来作为每一个要被逆转的子链表的头，亦即逆转后的尾巴
 int main(void)
 {
 	int link,link_len,K;
-	int tmp_link=-2,tmp,count;
+	int tmp_link=-2,tmp,count;//因为tmp_link会在Reverse内被改变，所以用tmp来记录它
 	int head=MaxSize;
 	link=RecieveLink(&link_len,&K);
 	for(count=0;count<link_len/K;count++)
 	{
-		if(tmp_link==-2)
+		if(tmp_link==-2)//tmp_link不等于-1的原因是它真的有可能是-1
 		{
 			link=Reverse(head,K,&tmp_link);
 		}
@@ -29,8 +29,6 @@ int main(void)
 			Array[tmp].Next=Reverse(head,K,&tmp_link);
 		}
 	}
-
-
 	PrintLink(link);
 	return 0;
 }
